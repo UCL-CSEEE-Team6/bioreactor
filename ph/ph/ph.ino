398a85f1f08f861a22934f5611df1064d0bf8054
ArtistYoon <43610352+ArtistYoon@users.noreply.github.com>
2018-12-03 15:57:17 +0000

    Update ph.ino

--- ph/ph/ph.ino
+++ ph/ph/ph.ino
@@ -1,10 +1,21 @@
-float analogdata;
+#include <math.h>
+#define ph   //This will be info from analog ph
+#define phS 7 //ph of standard solution
+#define Es   //Electrical potential at reference or standard electrode
+#define F 9.6485309*10000 //Faraday constant
+#define R 8.314510 //universal gas constant
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(ph,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int Ex = analogRead(ph);
+  int T = ;//This will get information from Temperature Team
+  int ln = log(10)/log(2.71828);
+  float phX = phS + (F*Es-F*Ex)/(R*T*ln);
+  Serial.println(phX);
 
 }
