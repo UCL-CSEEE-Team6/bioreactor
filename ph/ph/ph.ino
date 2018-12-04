c25469d01c9a0dd9aaabc13e3271f4350aba2033
johnnyjiang1 <jtc20000729@gmail.com>
2018-12-04 21:00:23 +0000

    Update ph.ino

--- ph/ph/ph.ino
+++ ph/ph/ph.ino
@@ -4,6 +4,12 @@
 #define Es   //Electrical potential at reference or standard electrode
 #define F 9.6485309*10000 //Faraday constant
 #define R 8.314510 //universal gas constant
+#define pump1  //pinNum of pump for acid
+#define pump2  //pinNum of pump for alkali
+
+bool acid;
+bool alkali;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -12,10 +18,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  char str[];
   int Ex = analogRead(ph);
   int T = ;//This will get information from Temperature Team
   int ln = log(10)/log(2.71828);
   float phX = phS + (F*Es-F*Ex)/(R*T*ln);
   Serial.println(phX);
-
+  str[]=Serial.read();
+  if (str[]=="") acid=True;
+  if (str[]=="") acid=False;
+  if (str[]=="") alkali=True;
+  if (str[]=="") alkali=False;
+  if (acid==True) digitalWrite(pump1,HIGH);
+  if (acid==False) digitalWrite(pump1,LOW);
+  if (alkali==True) digitalWrite(pump2,HIGH);
+  if (alkali==False) digitalWrite(pump2,LOW);
 }
