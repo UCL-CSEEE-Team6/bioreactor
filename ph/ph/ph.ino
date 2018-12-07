dd9fee9b633d03c150a49e708147e0695e4a87c3
O0OMichael <35950511+O0OMichael@users.noreply.github.com>
2018-12-07 11:48:19 +0000

    Update ph.ino

--- ph/ph/ph.ino
+++ ph/ph/ph.ino
@@ -1,12 +1,12 @@
 #include <math.h>
-#define ph A5 //This will be info from analog ph
-#define phS 7 //ph of standard solution
-#define Es 512  //Electrical potential at reference or standard electrode
-#define F 9.6485309*10000 //Faraday constant
-#define R 8.314510 //universal gas constant
-#define pump1 13 //pinNum of pump for acid
-#define pump2 12 //pinNum of pump for alkali
-#define voltageSupply 11 //pinNum of voltage supply of 1.024V
+const int ph=A5; //This will be info from analog ph
+const int phS=7; //ph of standard solution
+const float Es=512.0;  //Electrical potential at reference or standard electrode
+const float F=9.6485309*10000; //Faraday constant
+const float R=8.314510; //universal gas constant
+const int pump1=13; //pinNum of pump for acid
+const int pump2=12; //pinNum of pump for alkali
+const int voltageSupply=11; //pinNum of voltage supply of 1.024V
 #define tmp 1 //pinNum for temperature input
 
 bool acid;
@@ -16,7 +16,6 @@ int phv;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(ph,OUTPUT);
   pinMode(pump1,OUTPUT);
   pinMode(pump2,OUTPUT);
   analogWrite(voltageSupply,210);
@@ -35,14 +34,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   char str;
   int phY=analogRead(ph);
-  if (phY>50) phv=phY;
-  float Ex = phv*5000/1023;
+  Serial.print("phY");
+  Serial.println(phY);
+  /*if (phY>50)*/ phv=phY;
+  Serial.print("phv");
+  Serial.println(phv);
+  float Ex = phv*5000.0/1023;
   //int T = getTemp(analogRead(tmp)); //This will get information from Temperature Team
-  int T=293.15;
+  float T=293.15;
   Serial.print("temp:");
   Serial.println(T);
   float ln = log(10)/log(2.71828);
-  float phX = phS + (F*Es-F*Ex)/(R*T*ln);
+  float phX = phS + ((Es-Ex)*0.001*F)/(R*T*ln);
   Serial.print("ph:");
   Serial.println(phX);
   str=Serial.read();
@@ -58,5 +61,5 @@ void loop() {
   if (acid==false) digitalWrite(pump1,LOW);
   if (alkali==true) digitalWrite(pump2,HIGH);
   if (alkali==false) digitalWrite(pump2,LOW);
-  delay(100);
+  delay(1000);
 }
